06ab6f58ed963af7bfdf9f6432cb9e59a7b55465
Shweta Shakapnor <shwetashakapnor1999@gmail.com>
2021-10-03 21:16:33 +0530

    Promise all is added and three images are retrieved

--- index.js
+++ index.js
@@ -27,24 +27,45 @@ function writeFilePromise(fileLocation, result) {
     });
   });
 }
+// async function getRandomPic() {
+//   try {
+//     const res = await superagent.get(`https://robohash.org/${randomString(6)}`);
+//     console.log("Random image is ", res.request.url);
+//     await writeFilePromise("./randomImage.txt", res.request.url);
+//     console.log("Sucessfully written the file");
+//   } catch (err) {
+//     throw err;
+//   }
+//   console.log("2. Complete");
+// }
+
 async function getRandomPic() {
   try {
-    const res = await superagent.get(`https://robohash.org/${randomString(6)}`);
-    console.log("Random image is ", res.request.url);
-    await writeFilePromise("./randomImage.txt", res.request.url);
+    const res = await superagent.get(`https://robohash.org/${randomString(8)}`);
+    const res2 = await superagent.get(
+      `https://robohash.org/${randomString(8)}`
+    );
+    const res3 = await superagent.get(
+      `https://robohash.org/${randomString(8)}`
+    );
+    //console.log('Random image is ', res.request.url)
+    const all = await Promise.all([res, res2, res3]); //to show all imgs details
+    const images = all.map((img) => img.request.url); // map holds key values
+    console.log(images);
+    await writeFilePromise("./randomImage.txt", images.join("\n")); // join for concatenating all elements in an array
     console.log("Sucessfully written the file");
   } catch (err) {
     throw err;
   }
-  console.log("2. Complete");
+  console.log("2. complete");
 }
 console.log("1. start");
 (async () => {
   try {
     await getRandomPic();
-    console.log("3. End");
+    console.log("3. end");
   } catch (err) {
-    console.log("3. End due to error");
+    console.log("3. error");
   }
 })();
 
