1724b9e603a97e9b6b9c71b69d7ccf14c2ff6d34
Shweta Shakapnor <shwetashakapnor1999@gmail.com>
2021-10-03 20:55:52 +0530

    Inside index.js Promise function is added

--- index.js
+++ index.js
@@ -15,15 +15,36 @@ function randomString(length) {
 
   return result;
 }
-
-// Callbacks
+//Promises
+function writeFilePromise(fileLocation, result) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(fileLocation, result, (err) => {
+      if (err) {
+        reject("not able to write to the file");
+      }
+      resolve();
+    });
+  });
+}
 superagent.get(`https://robohash.org/${randomString(6)}`).end((err, res) => {
   console.log(res.request.url);
-  fs.writeFile("./randomImage.txt", res.request.url, (err) => {
-    if (err) {
-      console.log("Not able to write image in txt file", err);
-      return;
-    }
-    console.log(`Random Image successfully written inside the file`);
-  });
+  return writeFilePromise("./randomImage.txt", res.request.url)
+    .then(() => {
+      console.log("Sucessfully written the file");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
+
+// Callbacks
+// superagent.get(`https://robohash.org/${randomString(6)}`).end((err, res) => {
+//   console.log(res.request.url);
+//   fs.writeFile("./randomImage.txt", res.request.url, (err) => {
+//     if (err) {
+//       console.log("Not able to write image in txt file", err);
+//       return;
+//     }
+//     console.log(`Random Image successfully written inside the file`);
+//   });
+// });
